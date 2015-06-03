cacbd326ec8c20a2f6f25cb8c9b18e6b051eb08c
jinjinwang <jinjinwang@jinjinwangtekiMac.local>
2015-06-03 06:18:05 -0700

    Initial Commit

---
+++ helloworld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  helloworld
+//
+//  Created by jinjinwang on 15-6-3.
+//  Copyright (c) 2015年 jinjinwang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
